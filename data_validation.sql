0aac4b0dc6cfe0655e64f0dc1a325b615ad1d1e9
Natasha Ely <110025441+natashaely@users.noreply.github.com>
2023-08-06 21:50:02 +0200

    Update data_validation.sql

--- data_validation.sql
+++ data_validation.sql
@@ -1,44 +1,58 @@
 -- Examining the structure of the dataset
 
-SELECT column_name, data_type
-FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.INFORMATION_SCHEMA.COLUMNS`
-WHERE table_name = 'events_20210131' 
+SELECT
+  column_name,
+  data_type
+FROM
+  `bigquery-public-data.ga4_obfuscated_sample_ecommerce.INFORMATION_SCHEMA.COLUMNS`
+WHERE
+  table_name = 'events_20210131'
 
   
 -- Counting the total number of rows in the table
 
-SELECT COUNT(*) AS total_rows
-FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+SELECT
+  COUNT(*) AS total_rows
+FROM
+  `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
 
 
 -- Counting the total number of columns in the table
 
-SELECT 
-    COUNT(*) AS total_columns
-FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.INFORMATION_SCHEMA.COLUMNS`
-WHERE table_name = 'events_20210131';
+SELECT
+  COUNT(*) AS total_columns
+FROM
+  `bigquery-public-data.ga4_obfuscated_sample_ecommerce.INFORMATION_SCHEMA.COLUMNS`
+WHERE
+  table_name = 'events_20210131';
 
 
 -- Querying a sample of the table’s data
-  
-SELECT *
-FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
-LIMIT 5
+SELECT
+  *
+FROM
+  `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+LIMIT
+  5  
 
 
 -- Viewing a random sample of the data
 
-SELECT *
-FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
-WHERE RAND() < 0.001
+SELECT
+  *
+FROM
+  `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+WHERE
+  RAND() < 0.001
 
 
 -- Counting the Distinct Values within the event_name column
 
-SELECT COUNT(DISTINCT event_name) AS distinct_values
-FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+SELECT
+  COUNT(DISTINCT event_name) AS distinct_values
+FROM
+  `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
 
-  
 -- Checking that all of the dates are in a consistent 'YYYYMMDD' format
 
 SELECT event_date
