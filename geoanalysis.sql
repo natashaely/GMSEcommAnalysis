903fe226c6b068621e492d3173391eca8a518230
Natasha Ely <110025441+natashaely@users.noreply.github.com>
2023-08-09 16:42:13 +0200

    Update geoanalysis.sql

--- geoanalysis.sql
+++ geoanalysis.sql
@@ -1,5 +1,5 @@
 -- Seasonality Analysis
--- Breaking down sales data on a monthly basis
+-- Breaking down sales data every month
 
 SELECT
   EXTRACT(MONTH
@@ -18,7 +18,7 @@ ORDER BY
   sale_month;
 
 
--- Breaking down sales data on a weekly basis
+-- Breaking down sales data every week
 
 SELECT
   EXTRACT(WEEK
@@ -38,6 +38,83 @@ ORDER BY
 
 
 -- Geographic Analysis
+
+-- Average item price per country
+
+SELECT
+  geo.country,
+  AVG(item.price_in_usd) AS avg_price
+FROM
+  `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`,
+  UNNEST(items) AS item
+GROUP BY
+  geo.country
+ORDER BY
+  avg_price DESC;
+  
+  
+-- The average basket size per country (in USD)
+
+WITH PurchaseTotals AS (
+  SELECT
+    geo.country,
+    SUM(item.price) AS total_purchase_value
+  FROM
+    `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*` events,
+    UNNEST(events.items) AS item
+  WHERE
+    event_name = 'purchase'
+  GROUP BY
+    events.event_bundle_sequence_id, -- Assuming this uniquely identifies a purchase
+    geo.country
+)
+
+SELECT
+  country,
+  AVG(total_purchase_value) AS avg_purchase_value_per_country
+FROM
+  PurchaseTotals
+GROUP BY
+  country
+ORDER BY
+  avg_purchase_value_per_country DESC;
+
+
+-- The most active and valuable customers per country 
+
+WITH BestSellers AS (
+  SELECT
+    item.item_id
+  FROM
+    `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`,
+    UNNEST(items) AS item
+  WHERE
+    event_name = 'purchase'
+  GROUP BY
+    item.item_id
+  ORDER BY
+    COUNT(*) DESC
+  LIMIT
+    10
+)
+
+SELECT
+  geo.country,
+  COUNT(*) AS count,
+  AVG(events.user_ltv.revenue) AS avg_ltv
+FROM
+  `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*` events,
+  UNNEST(events.items) AS item
+JOIN
+  BestSellers
+ON
+  item.item_id = BestSellers.item_id
+GROUP BY
+  geo.country
+ORDER BY
+  count DESC;
+
+
 -- Storing geographic information by country 
 
 SELECT
